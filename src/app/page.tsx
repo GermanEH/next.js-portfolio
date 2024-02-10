2f0b17a811d7568eab5dc2c0c08f62b9a6fda6b7
EzequielYDev <151093227+EzequielYDev@users.noreply.github.com>
2024-02-10 12:00:30 -0300

    Feat. Add CV in english for downloading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-10">
-    <div className="relative flex flex-col justify-center items-start mr-52 mb-20">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <div className="max-w-5xl w-full flex justify-between items-center pb-20">
+      <div className="flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/profile_photo_no_background.jpg"
@@ -14,9 +14,16 @@ export default function Home() {
           priority
         />
       <h2 className="ml-2 font-bold">Germán Horianski</h2>
-      <div className="ml-96">
+      {/* <div className="ml-96">
+      </div> */}
       </div>
-        <p className="text-lg font-semibold">Download CV</p>
+      <div>
+        <a 
+        href="/CV Germán Horianski (english).pdf" 
+        target="_blank" 
+        className="text-lg font-semibold" 
+        rel="noopener noreferrer"><h2>Download CV</h2>
+        </a>
       </div>
     </div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono lg:flex">
@@ -47,7 +54,6 @@ export default function Home() {
             Find in-depth information about Next.js features and API.
           </p>
         </a> */}
-
         <a
           // href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
