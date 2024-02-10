03d4ec24d1deee34559648fc7cfa28168c3af095
EzequielYDev <151093227+EzequielYDev@users.noreply.github.com>
2024-02-10 16:02:37 -0300

    Feat. Add basic Projects section

---
+++ src/app/ui/Projects.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import {useState, useEffect} from 'react'
+
+export default function Projects() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsVisible(true);
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+  return (
+    <>
+      {isVisible && (<div className="z-10 max-w-5xl w-full h-110 font-mono lg:flex">
+        {/* <p className="text-8xl fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> */}
+        <p className="text-4xl flex w-full justify-center lg:static lg:p-4 border rounded-lg border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30">
+          Projects
+        </p>
+        {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        </div> */}
+      </div>
+      )}
+    </>
+  );
+}
