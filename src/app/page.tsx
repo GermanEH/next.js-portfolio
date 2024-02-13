6143671fb6aafb0378fa67673c0b1829a2a64e9d
GermanEH <germanhdev@gmail.com>
2024-02-13 14:52:15 -0300

    Perf. Refactor page.tsx state management with a custom hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,14 @@
 'use client'
 
-import {useState} from "react"
 import Header from './ui/Header'
 import HeroText from "./ui/HeroText";
 import Card from './ui/Card'
 import CardsList from "./ui/CardsList";
+import useClickedState from './hooks/useClickedState'
 
 export default function Home() {
 
-  interface MyState {
-    [key: string]: boolean;
-  }
-
-  const [isClickedState, setClickedState] = useState<MyState>({
-    isAboutClicked: false,
-    isProjectsClicked: false,
-    isContactClicked: false
-  })
-
-  const handleClickedState = (itemClicked: string) => {
-    setClickedState(prevState => {
-      const updatedState: MyState = {...prevState}
-      for (const key in prevState) {
-        if(key === itemClicked) {
-          updatedState[key] = !prevState[key]
-           } else {
-          updatedState[key] = false
-        }
-      }
-      return updatedState
-    })
-    }
+  const { isClickedState, handleClickedState } = useClickedState();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-10">
