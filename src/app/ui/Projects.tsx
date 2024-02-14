009185700522bbab5203a3702d0ab5d2fca3e94f
GermanEH <germanhdev@gmail.com>
2024-02-14 10:26:18 -0300

    Perf. Change img tag for Image component

--- src/app/ui/Projects.tsx
+++ src/app/ui/Projects.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import {useState, useEffect} from 'react'
+import Image from "next/image";
 
 export default function Projects() {
   const [isVisible, setIsVisible] = useState(false);
@@ -17,19 +18,19 @@ export default function Projects() {
       <>
       <div className={`absolute lg:max-w-5xl lg:w-full pl-20 pt-12 lg:mt-48 z-20 transition-opacity opacity-${isVisible ? '100' : '0'} duration-1000 flex flex-wrap `}>
        <div className='flex flex-col justify-content items-center'>
-       <img src="/D&D_logo.jpg" height={'250px'} width={'250px'}/>
+       <Image src="/D&D_logo.jpg" alt="project_image" height={250} width={250}/>
        <p className='mr-4 mt-4 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className='flex flex-col justify-content items-center'>
-        <img src="/D&D_logo.jpg" height={'250px'} width={'250px'}/>
+        <Image src="/D&D_logo.jpg" alt="project_image" height={250} width={250}/>
         <p className='mr-4 mt-4 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className='flex flex-col justify-content items-center'>
-        <img src="/D&D_logo.jpg" height={'250px'} width={'250px'}/>
+        <Image src="/D&D_logo.jpg" alt="project_image" height={250} width={250}/>
         <p className='mr-4 mt-4 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className='flex flex-col justify-content items-center'>
-        <img src="/D&D_logo.jpg" height={'250px'} width={'250px'}/>
+        <img src="/D&D_logo.jpg" alt="project_image" height={250} width={250}/>
         <p className='mr-4 mt-4 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
       </div>
