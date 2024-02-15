5379b4ade3a0ba9379c09330f90b50ddbfdfd2a7
GermanEH <germanhdev@gmail.com>
2024-02-15 12:58:02 -0300

    Chore. Implement dictionary for CardsList component

--- app/[lang]/components/CardsList.tsx
+++ app/[lang]/components/CardsList.tsx
@@ -1,15 +1,23 @@
+'use client'
+
 import CardFlip from "./CardFlip";
-interface CardsListProps {
-    isClickedState: any;
-    handleClickedState: any;
-  }
-const CardsList:React.FC<CardsListProps> = ({handleClickedState, isClickedState}) => {
+import useClickedState from '../hooks/useClickedState'
+import { SectionProps } from "./Card";
+import Card from './Card'
+
+const CardsList:React.FC<SectionProps> = ({dictionary}) => {
+
+  const { isClickedState, handleClickedState } = useClickedState();
+  const {section_1_title, section_1_subtitle, section_2_title, section_2_subtitle, section_3_title, section_3_subtitle} = dictionary
   return (
+    <>
+      <Card isClickedState={isClickedState} dictionary={dictionary}/>
     <div className={`grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left pb-20`}>
-    <CardFlip title='About me' subtitle='Learn about my academic background!' isClickedState={isClickedState} handleClickedState={handleClickedState} clickedSection={'isAboutClicked'}/>
-    <CardFlip title='Projects' subtitle="See what I've built." isClickedState={isClickedState} handleClickedState={handleClickedState} clickedSection={'isProjectsClicked'}/>
-    <CardFlip title='Contact' subtitle="Let's connect!" isClickedState={isClickedState} handleClickedState={handleClickedState} clickedSection={'isContactClicked'}/>
+      <CardFlip title={section_1_title} subtitle={section_1_subtitle} isClickedState={isClickedState} handleClickedState={handleClickedState} clickedSection={'isAboutClicked'}/>
+      <CardFlip title={section_2_title} subtitle={section_2_subtitle} isClickedState={isClickedState} handleClickedState={handleClickedState} clickedSection={'isProjectsClicked'}/>
+      <CardFlip title={section_3_title} subtitle={section_3_subtitle} isClickedState={isClickedState} handleClickedState={handleClickedState} clickedSection={'isContactClicked'}/>
   </div> 
+    </>
   )
 }
 
