d3207d19705fc4e362569cf03af5a5498334a070
GermanEH <germanhdev@gmail.com>
2024-02-19 14:01:47 -0300

    Fix. Delete background effect for small screens

--- app/[lang]/components/Header.tsx
+++ app/[lang]/components/Header.tsx
@@ -7,8 +7,12 @@ const Header = ({CV}:CV) => {
     <>
       <div className="max-w-5xl w-full flex justify-between items-center px-4 sm:px-20 lg:px-0 pb-20">
       <div className="flex place-items-center 
-      before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] 
-      after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
+      before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial 
+      before:from-white before:to-transparent before:blur-2xl before:content-[''] 
+      after:absolute after:-z-20 after:none md:after:h-[180px] after:overflow-hidden after:w-full sm:after:w-[240px] after:translate-x-1/3 
+      after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] 
+      before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 
+      before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
       >
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
