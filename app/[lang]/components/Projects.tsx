d59718fd0f35f823adbec87c99d6caa292927e34
GermanEH <germanhdev@gmail.com>
2024-02-20 21:09:27 -0300

    Styling. Narrow the Projects component card

--- app/[lang]/components/Projects.tsx
+++ app/[lang]/components/Projects.tsx
@@ -17,22 +17,32 @@ const Projects:React.FC<SectionProps> = ({dictionary}) => {
   return (
     <>
       {isVisible && 
-      <div className={`absolute lg:max-w-5xl lg:w-full pl-20 pt-12 mt-[28vh] mobile-s:mt-[18vh] mobile-s:mr-[20vw] sm:mt-[36vh] mobile-m:mt-[12vh] mobile-m:mr-[10vw] mobile-hd:mt-[44vh] mobile-hd:ml-[15vw] md:mt-[56vh] lg:mt-[34vh] desktop-hd:mt-[32] xl:mt-[28vh] desktop-full-hd:mt-[34vh] z-20 transition-opacity opacity-${isVisible ? '100' : '0'} duration-1000 flex flex-wrap mobile-s:max-xl:scale-75 lg:max-xl:ml-[20vw] `}>
-       <div className='flex flex-col justify-content items-center'>
+      <div className={`absolute lg:max-w-5xl mobile-s:w-full pl-20 pt-12 mt-[28vh] 
+      mobile-s:mt-[18vh] mobile-s:mr-[18vw] sm:mt-[36vh] mobile-m:mt-[12vh] mobile-m:mr-[10vw] mobile-l:max-sm:pl-[1vw] mobile-l:max-sm:mr-[0]
+      sm:max-lg:ml-[20vw] sm:max-md:mt-[28vh] 
+      mobile-hd:max-lg:pl-[16vw]
+      desktop-hd:mt-[32] 
+      lg:mt-[28vh] lg:pl-2 lg:max-2xl:ml-[20vw] lg:scale-[80%] 
+      xl:mt-[24vh] xl:scale-[95%]
+      2xl:mt-[28vh] 2xl:ml-[28vh]
+      desktop-full-hd:mt-[28vh] desktop-full-hd:ml-[36vh]
+      z-20 transition-opacity opacity-${isVisible ? '100' : '0'} duration-1000 flex flex-wrap 
+      mobile-s:max-md:scale-75 `}>
+       <div className='flex flex-col items-center'>
        <Image src="/D&D_logo.jpg" alt="project_image" height={250} width={250}/>
-       <p className='mr-4 mt-4 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+       <p className='mr-4 mt-4 mb-4 mobile-s:max-sm:mr-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className='flex flex-col items-center'>
+        <Image src="/D&D_logo.jpg" alt="project_image" height={250} width={250}/>
+        <p className='mr-4 mt-4 mb-4 mobile-l:max-sm:mr-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className='flex flex-col justify-content items-center'>
         <Image src="/D&D_logo.jpg" alt="project_image" height={250} width={250}/>
-        <p className='mr-4 mt-4 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-        <div className='flex flex-col justify-content items-center'>
-        <Image src="/D&D_logo.jpg" alt="project_image" height={250} width={250}/>
-        <p className='mr-4 mt-4 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p className='mr-4 mt-4 mb-4 mobile-l:max-sm:mr-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className='flex flex-col justify-content items-center'>
         <img src="/D&D_logo.jpg" alt="project_image" height={250} width={250}/>
-        <p className='mr-4 mt-4 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p className='mr-4 mt-4 mb-4 mobile-l:max-sm:mr-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
       </div>
       }
