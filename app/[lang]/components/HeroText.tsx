59a6e8a91d9b43708be8f6f3dc7fdb4e0871bf9d
GermanEH <germanhdev@gmail.com>
2024-02-20 21:11:31 -0300

    Styling. Add padding-right to HeroText

--- app/[lang]/components/HeroText.tsx
+++ app/[lang]/components/HeroText.tsx
@@ -7,15 +7,15 @@ export default function HeroText({title_1, title_2}: title) {
 
   return (
     <>
-      <div className="max-w-5xl w-full items-center font-mono lg:flex">
+      <div className="max-w-5xl w-full items-center font-mono lg:flex mobile-s:max-sm:scale-[80%]">
         {/* <p className="text-8xl fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> */}
-        <p className="text-8xl left-0 top-0 flex w-full lg:static lg:w-auto p-4 mobile-hd:px-20 lg:px-28">
+        <p className="text-8xl left-0 top-0 flex w-full lg:static lg:w-auto p-4 lg:px-28 mobile-m:pl-8 sm:pl-24">
           {title_1}
         </p>
         {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         </div> */}
       </div>
-      <div className="w-full max-w-5xl pb-16 mobile-hd:max-lg:px-20">
+      <div className="w-full max-w-5xl pb-16 mobile-s:pb-0 mobile-l:pr-16 mobile-hd:max-lg:px-20 mobile-s:max-sm:scale-[80%]">
         <p className="text-2xl w-full pl-28 lg:pl-48">{title_2}</p>
       </div>
     </>
