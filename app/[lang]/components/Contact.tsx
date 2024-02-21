03ab953270df73fd71bfd8872b11bb1bda65855e
GermanEH <germanhdev@gmail.com>
2024-02-20 21:09:45 -0300

    Styling. Narrow the Contact component card

--- app/[lang]/components/Contact.tsx
+++ app/[lang]/components/Contact.tsx
@@ -16,8 +16,18 @@ const Contact:React.FC<SectionProps> = ({dictionary}) => {
   return (
     <>
     {isVisible  && 
-      <div className={`absolute lg:max-w-5xl lg:w-full ml-20 lg:ml-0 p-4 mt-[28vh] mobile-s:mt-[24vh] mobile-s:mr-[20vw] sm:mt-[44vh] mobile-hd:mt-[50vh] mobile-hd:ml-[25vw] md:mt-[56vh] lg:mt-[34vh] desktop-hd:mt-[26vh] xl:mt-[28vh] desktop-full-hd:mt-[34vh] z-20 transition-opacity opacity-${isVisible ? '100' : '0'} duration-1000 items-center lg:grid lg:grid-cols-2 lg:max-xl:ml-[24vw] items-center mobile-s:max-xl:scale-75`}>
-        <section className='flex flex-col justify-center items-center'>
+      <div className={`absolute lg:max-w-5xl lg:w-full ml-20 lg:ml-0 p-4 mt-[28vh] 
+      mobile-s:mt-[24vh] mobile-s:mr-[20vw] 
+      sm:mt-[40vh] sm:ml-[21vw] 
+      mobile-hd:mt-[50vh] mobile-hd:ml-[25vw] 
+      lg:mt-[32vh] lg:max-2xl:ml-[24vw]
+      desktop-hd:mt-[26vh] 
+      xl:mt-[24vh]
+      2xl:mt-[28vh] 2xl:ml-[32vh]
+      desktop-full-hd:mt-[28vh] desktop-full-hd:ml-[38vh]
+      z-20 transition-opacity opacity-${isVisible ? '100' : '0'} duration-1000 items-center 
+      lg:grid lg:grid-cols-2  items-center mobile-s:scale-75 sm:scale-[90%]`}>
+        <section className='flex flex-col justify-center items-center sm:max-md:mt-[5vh]'>
           <div className="w-72 h-24 mb-2 flex flex-col justify-center items-center border dark:border-neutral-700 dark:bg-slate-900">
             <img src="/logo.png" height={'50px'} width={'50px'}></img>
             <p>Mar del Plata, Argentina</p>
@@ -32,7 +42,7 @@ const Contact:React.FC<SectionProps> = ({dictionary}) => {
             </div>
         </section>
         <section className='flex flex-col justify-center items-center'>
-        <h2 className="text-xl font-bold mt-16 lg:mt-0 mb-4">{dictionary?.section_3_body_1}</h2>
+        <h2 className="text-xl font-bold mt-16 mobile-s:mt-8 sm:mt-[10vh] lg:mt-0 mb-4">{dictionary?.section_3_body_1}</h2>
           <form className='flex flex-col justify-center items-center'>
             <input type="text" placeholder="Email" className='p-2 m-4 rounded text-black' />
             <input type="text" placeholder="Full Name" className='p-2 m-4 rounded text-black'/>
